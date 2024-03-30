3e317fb68947bac46e7d2ea0eee5cb4343bcd121
VictorF <victorfntlb@gmail.com>
2024-03-30 16:00:46 -0400

    starting to add buttons

--- WpfApp1/ViewModels/ItemIdentificationViewModel.cs
+++ WpfApp1/ViewModels/ItemIdentificationViewModel.cs
@@ -1,11 +1,25 @@
-﻿namespace WpfApp1.ViewModels
+﻿using System.ComponentModel;
+
+namespace WpfApp1.ViewModels
 {
     public class ItemIdentificationViewModel : ViewModelBase
     {
         public ItemIdentificationViewModel(AppStateVM appState)
         {
             AppState = appState;
+            ItemOptions = new List<ItemOption>
+            {
+                new() { Name = "Test" }
+            };
         }
         public AppStateVM AppState { get; set; }
+
+        public List<ItemOption> ItemOptions { get; set; }
+
+    }
+
+    public class ItemOption
+    {
+        public string Name { get; set; } = string.Empty;
     }
 }
