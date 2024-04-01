802a4221f9c26d79fa29389b1ef680d477c1a849
VictorF <victorfntlb@gmail.com>
2024-04-01 14:03:02 -0400

    added cases and frenchified application

--- WpfApp1/ViewModels/ItemIdentificationViewModel.cs
+++ WpfApp1/ViewModels/ItemIdentificationViewModel.cs
@@ -9,18 +9,48 @@ namespace WpfApp1.ViewModels
             AppState = appState;
             ItemOptions = new List<ItemOption>
             {
-                new() { Name = ItemOptionEnum.Fruit },
-                new() { Name = ItemOptionEnum.Vegetable },
-                new() { Name = ItemOptionEnum.Meat },
-                new() { Name = ItemOptionEnum.Snacks },
-                new() { Name = ItemOptionEnum.SnackEnclosure },
-                new() { Name = ItemOptionEnum.Recycling1 },
-                new() { Name = ItemOptionEnum.Recycling2 },
-                new() { Name = ItemOptionEnum.Recycling3 },
-                new() { Name = ItemOptionEnum.Recycling4,},
-                new() { Name = ItemOptionEnum.Recycling5 },
-                new() { Name = ItemOptionEnum.Recycling6 },
-                new() { Name = ItemOptionEnum.Recycling7 },
+                new() { Name = ItemOptionEnum.Fruit ,
+                Description = "Fruits"},
+                new() { Name = ItemOptionEnum.Vegetable ,
+                Description = "Légumes"},
+                new() { Name = ItemOptionEnum.Meat ,
+                Description = "Viande"},
+                new() { Name = ItemOptionEnum.Snacks ,
+                Description = "Collations"},
+                new() { Name = ItemOptionEnum.SnackEnclosure,
+                Description = "Sachet de chips"},
+                new() { Name = ItemOptionEnum.Recycling1 ,
+                Description = "Plastique 1"},
+                new() { Name = ItemOptionEnum.Recycling2 ,
+                Description = "Plastique 2"},
+                new() { Name = ItemOptionEnum.Recycling3 ,
+                Description = "Plastique 3"},
+                new() { Name = ItemOptionEnum.Recycling4,
+                Description = "Plastique 4"},
+                new() { Name = ItemOptionEnum.Recycling5,
+                Description = "Plastique 5"},
+                new() { Name = ItemOptionEnum.Recycling6,
+                Description = "Plastique 6"},
+                new() { Name = ItemOptionEnum.Recycling7,
+                Description = "Plastique 7"},
+                new() { Name = ItemOptionEnum.BrownPaper,
+                Description = "Papier brun"},
+                new() { Name = ItemOptionEnum.WetCardboard,
+                Description = "Carton souillé"},
+                new() { Name = ItemOptionEnum.Tissues,
+                Description = "Tissues"},
+                new() { Name = ItemOptionEnum.Alluminium,
+                Description = "Alluminium"},
+                new() { Name = ItemOptionEnum.Paper,
+                Description = "Papier"},
+                new() { Name = ItemOptionEnum.Cardboard,
+                Description = "Carton"},
+                new() { Name = ItemOptionEnum.Glass,
+                Description = "Verre"},
+                new() { Name = ItemOptionEnum.NonRecyclable,
+                Description = "Non recyclable"},
+                new() { Name = ItemOptionEnum.Unknown,
+                Description = "Inconnu"},
             };
             SelectGarbageCommand = new CommitToItemCommand(this);
         }
@@ -53,11 +83,12 @@ namespace WpfApp1.ViewModels
             }
         }
     }
-    
+
 
     public class ItemOption
     {
         public ItemOptionEnum Name { get; set; }
+        public string Description { get; set; }
     }
 
     public enum ItemOptionEnum
@@ -75,5 +106,13 @@ namespace WpfApp1.ViewModels
         Meat,
         Snacks,
         SnackEnclosure,
+        BrownPaper,
+        WetCardboard,
+        Tissues,
+        Alluminium,
+        Paper,
+        Cardboard,
+        Glass,
+        NonRecyclable,
     }
 }
