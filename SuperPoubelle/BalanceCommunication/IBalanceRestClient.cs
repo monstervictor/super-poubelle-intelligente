c3f548eed2e330a756a640e2c5b75a75363a2f1b
VictorF <victorfntlb@gmail.com>
2024-04-24 12:33:35 -0400

    added config for IP + client - not used

---
+++ SuperPoubelle/BalanceCommunication/IBalanceRestClient.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+
+namespace SuperPoubelle.BalanceCommunication
+{
+    public interface IBalanceRestClient
+    {
+        [Get("/balance")]
+        Task<IEnumerable<BalanceReport>> GetBalanceReports();
+
+        [Post("/balance/{id}/calibrate")]
+        Task Calibrate([AliasAs("id")] string balanceId, [Body] CalibrationBody body);
+    }
+
+    public class BalanceReport
+    {
+        public string BalanceIdentifier { get; set; }
+        public double Grams { get; set; }
+    }
+
+    public class CalibrationBody
+    {
+        public double Grams { get; set; }
+    }
+}
