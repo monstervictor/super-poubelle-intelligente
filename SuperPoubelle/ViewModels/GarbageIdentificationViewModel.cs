32367425b1af9bab29a1fb538ce64ce777d2d032
VictorF <victorfntlb@gmail.com>
2024-04-24 16:14:13 -0400

    missind how to make the decision

--- SuperPoubelle/ViewModels/GarbageIdentificationViewModel.cs
+++ SuperPoubelle/ViewModels/GarbageIdentificationViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System.Windows.Input;
+﻿using SuperPoubelle.BalanceCommunication;
+using System.Windows.Input;
 
 namespace SuperPoubelle.ViewModels
 {
     public class GarbageIdentificationViewModel : ViewModelBase
     {
+        private bool _isMonitoring;
+
         public GarbageIdentificationViewModel(AppStateVM appState)
         {
             AppState = appState;
@@ -35,8 +38,40 @@ namespace SuperPoubelle.ViewModels
 
         public void PerformSelectBin(BinSelection selection)
         {
+            _isMonitoring = false;
             AppState.SelectedBin = selection;
             NextCommand.Execute(null);
+        }
+
+        internal void BeginMonitoringSelection()
+        {
+            if (_isMonitoring) return;
+            _isMonitoring = true;
+            Task.Run(() => FetchBalances());
+        }
+
+        private Stack<IEnumerable<BalanceReport>> _data = new Stack<IEnumerable<BalanceReport>>();
+
+        private async Task FetchBalances()
+        {
+            _isMonitoring = true;
+            do
+            {
+                var balances = await BalanceClient.Instance.GetBalanceReports();
+                if (!_data.TryPeek(out var topOfStack))
+                {
+                    _data.Push(balances);
+                    continue;
+                }
+
+                // Magic
+                //PerformSelectBin(BinSelection.Compost);
+
+                _data.Push(balances);
+                await Task.Delay(500);
+
+            } while (_isMonitoring);
+            _data.Clear();
         }
     }
 
